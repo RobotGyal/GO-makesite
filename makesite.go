48e4c2cfea5e0db94b2e3f972e3931b6c838a479
Aleia <aleiaknight@gmail.com>
2020-06-28 18:47:31 -0700

    mvp

--- makesite.go
+++ makesite.go
@@ -1,32 +1,59 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
 	"html/template"
 	"io/ioutil"
-	"os"
+	"strings"
 )
 
-func main() {
-	readFile()
+type post struct {
+	Author  string
+	Content string
 }
 
-func readFile() string {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+func readFile(file string) string {
+	fileContents, err := ioutil.ReadFile(file)
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Print(string(fileContents))
+	fmt.Print(string(fileContents))
 	return string(fileContents)
 }
 
-func renderTemplate() {
+func renderTemplate(content string) string {
 	paths := []string{
 		"template.tmpl",
 	}
 
-	t := template.Must(template.New("html-tmpl").ParseFiles(paths...))
-	err = t.Execute(os.Stdout, makesite)
+	t := template.Must(template.New("template.tmpl").ParseFiles(paths...))
+	buffer := new(bytes.Buffer)
+	err := t.Execute(buffer, post{Author: "Aleia", Content: "Random Content for latest post"})
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(buffer.String())
+	return buffer.String()
+}
+
+func writeFile(buffer string, file string) bool {
+	bytes := []byte(buffer)
+	err := ioutil.WriteFile(file, bytes, 0644)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func main() {
+	// FlAGS
+	filePtr := flag.String("file", "latest-post.txt", "displays file content")
+	flag.Parse()
+
+	// readFile("first-post.txt")
+	template := renderTemplate(*filePtr)
+	namedFile := strings.SplitN(*filePtr, ".", 2)[0] + ".html"
+	writeFile(template, namedFile)
 }
